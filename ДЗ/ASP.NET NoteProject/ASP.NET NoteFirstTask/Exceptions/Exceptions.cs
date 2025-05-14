6fb7561e6aaf0b932f5dd9a25ea6776714341508
AlexsunderGIT <alexgo3228@gmail.com>
2025-05-14 15:37:39 +0500

    +FirstTaskNote

---
+++ ДЗ/ASP.NET NoteProject/ASP.NET NoteFirstTask/Exceptions/Exceptions.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleProject.NET.Exceptions
+{
+    public class NoteNotFoundException : Exception
+    {
+        public NoteNotFoundException() : base($"Note not found") { }
+    }
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base($"User not found") { }
+    }
+
+}
