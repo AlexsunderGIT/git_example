76818bdedfd1934533d17397c37b1c61800fdb50
AlexsunderGIT <alexgo3228@gmail.com>
2025-05-29 14:23:41 +0500

    +29.05 start

---
+++ ДЗ/ASP.NET NoteProject/ASP.NET NoteThirdTask/Composer.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using ConsoleProject.NET;
+using ConsoleProject.NET.Repositories;
+using ConsoleProject.NET.Services;
+
+namespace SimpleExample;
+     public static class Composer
+     {   
+            // Тут будем добавлять инфраструктурные сервисы 
+            // (которые нужны для передачи данных внутри приложения).
+            public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+            {
+
+                // Добавляем автомаппер и регистрируем в нем все классы из
+                // нашего проекта, которые мы наследовали от Profile
+                services.AddAutoMapper(typeof(Composer).Assembly);
+                services.AddExceptionHandler<ExceptionHandler>();
+                services.AddControllers();
+                services.AddSingleton<IUserRepository, UserRepository>();
+                services.AddSingleton<INoteRepository, NoteRepository>();
+                return services;
+    }
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+ }
---
+++ ДЗ/ASP.NET NoteProject/ASP.NET NoteThirdTask/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+using ConsoleProject.NET.Exceptions;
+using ConsoleProject.NET.Models;
+
+namespace ConsoleProject.NET.Repositories;
+
+public class UserRepository : IUserRepository   
+{
+    private readonly List<User> _users = new();
+    private int _idCounter;
+    public IReadOnlyList<User> GetUsers() => _users;
+    public User GetById(int id) 
+    {
+        //
+        var user = _users.FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            throw new UserNotFoundException();
+        }
+        return user;
+    }
+    public int Add(User user)
+    {
+        //
+        if (string.IsNullOrWhiteSpace(user.Name))
+            throw new NameIsRequired();
+        //
+        user.Id = ++_idCounter;
+        _users.Add(user);
+        return user.Id;
+    }
+}
