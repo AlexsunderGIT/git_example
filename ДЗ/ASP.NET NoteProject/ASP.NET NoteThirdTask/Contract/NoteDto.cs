05cdd3ee897d7e896ad06cf1a131c613e7804c5c
AlexsunderGIT <alexgo3228@gmail.com>
2025-05-29 14:22:51 +0500

    +huyna

--- ДЗ/ASP.NET NoteProject/ASP.NET NoteThirdTask/Contract/NoteDto.cs
+++
@@ -1,7 +0,0 @@
-using ConsoleProject.NET.Models;
-
-namespace ConsoleProject.NET.Contract;
-
-public record NoteAddDto(string Title, string Description, Priority priority, int UserId);
-
-public record NoteUpdateDto(string? Title, string? Description, Priority? priority, bool? IsCompleted);
--- ДЗ/ASP.NET NoteProject/ASP.NET NoteThirdTask/Contract/UserAddDto.cs
+++
@@ -1,4 +0,0 @@
-namespace ConsoleProject.NET.Contract;
-
-public record UserAddDto(string Name, string Password);
-public record UserVm(int id, string Name);
