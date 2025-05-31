789b6073b21faada4cfe0457de2162e729e1183a
AlexsunderGIT <alexgo3228@gmail.com>
2025-05-31 16:19:49 +0500

    +finalFIX

--- ДЗ/ASP.NET NoteProject/ASP.NET NoteThirdTask/Controllers/NoteController.cs
+++ ДЗ/ASP.NET NoteProject/ASP.NET NoteThirdTask/Controllers/NoteController.cs
@@ -14,7 +14,7 @@ public class NoteController(INoteRepository noteRepository) : ControllerBase
 {
     [HttpGet("user/{userId}")]
     public ActionResult<IReadOnlyList<NoteVM>> GetByUser(int userId) 
-    => Ok(noteRepository.GetByUserId);
+    => Ok(noteRepository.GetByUserId(userId));
     [HttpGet("{id}")]
     public ActionResult<NoteVM> GetById(int id)
     {
